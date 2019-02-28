3b10abfe1d2927b2fab9dd7991d88b176cf14c29
Bwitz <guitarrook94@gmail.com>
2019-02-27 23:40:21 -0500

    Added base functionality of homepage with state, two handlers for color change of title and select option, and imported previous css settings

--- src/Moodpages/Homepage.js
+++ src/Moodpages/Homepage.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 
 export default class Homepage extends Component {
+
+    state = {
+        currentMood: ""
+    }
+
+    changeHandler = (event)=> {
+        this.setState({
+            currentMood: event.target.value
+        })
+    }
+
+    titleHandler = () => {
+        switch(this.state.currentMood) {
+          case 'Happy':
+            return "title happyTitle"
+          case 'Sad':
+            return "title sadTitle"
+          case 'Bored':
+            return "title boredTitle"
+          default:
+            return "title"
+        }
+      }
   
     render() {
         return(
             <div>
-              <p> I'm the homepage! </p>  
+                <div className="MoodSelect">
+                    <h1 className={this.titleHandler()}><u>MoodPerker</u></h1>
+                    <br />
+                    <h3 className="secondaryTitle">How are you?</h3>
+                    <select className = 'select-thingy' value={this.state.currentMood} onChange={event => this.changeHandler(event)}>
+                    <option value="">I feel..</option>
+                    <option value="Happy" name="Happy"> Happy </option>
+                    <option value="Sad" name="Sad"> Sad </option>
+                    <option value="Bored" name="Bored"> Bored </option>
+                    </select>
+                </div>
             </div>
         )
     }
