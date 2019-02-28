175eea158681397bc19b505a84b66a7250b7f929
Bwitz <guitarrook94@gmail.com>
2019-02-27 23:25:35 -0500

    Created the baseline of folders that were used within moodperker v1

---
+++ src/Moodpages/Homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Homepage extends Component {
+  
+    render() {
+        return(
+            <div>
+              <p> I'm the homepage! </p>  
+            </div>
+        )
+    }
+
+}
